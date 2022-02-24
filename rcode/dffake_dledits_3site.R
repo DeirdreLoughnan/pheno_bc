a29f00591165c7e0061e8d7e2afe39808bcc6229
DeirdreLoughnan <deirdre.loughnan@gmail.com>
2022-02-24 12:57:22 -0800

    five chill port working

--- rcode/dffake_dledits_3site.R
+++ rcode/dffake_dledits_3site.R
@@ -34,7 +34,7 @@ require(lme4)
 #2. change site number, initially try having 3 sites
 # <> <> <> <> <> <> <> <> <> <> <> <> <> <> <> <> <> <> <> <>
 
-nsite = 2
+nsite = 3
 nsp = 28
 
 nwarm = 2
@@ -51,6 +51,7 @@ site = gl(nsite, rep, length = ntot)
 warm = gl(nwarm, rep*nsite, length = ntot)
 photo = gl(nphoto, rep*nsite*nwarm, length = ntot)
 chill = gl(nchill, rep*nsite*nwarm*nphoto, length = ntot)
+#chillP <- rep(c(10,20), each = rep*nsite*nwarm*nphoto)
 chillP <- rep(c(10,15,20,25,30), each = rep*nsite*nwarm*nphoto)
 
 #chill1 = ifelse(chill == 2, 1, 0) 
@@ -60,8 +61,8 @@ chillP <- rep(c(10,15,20,25,30), each = rep*nsite*nwarm*nphoto)
 treatcombo = paste(warm, photo, chill, sep = "_")
 
 # setting up the dummy variables for site:
-# site2 = ifelse(site == 2, 1, 0) 
-# site3 = ifelse(site == 3, 1, 0) 
+# site2 = ifelse(site == 2, 1, 0)
+# site3 = ifelse(site == 3, 1, 0)
 
 #d <- data.frame(site, warm, photo, chill, chill1, chill2, site2, site3, treatcombo) # critical coding error here!
 d <- data.frame(site, warm, photo, chill,  treatcombo) # critical coding error here!
@@ -118,6 +119,7 @@ mm <- model.matrix(~(site+ warm + photo + chillP)^2, data.frame(site, warm, phot
 #mm <- mm[,-grep("site2:site3", colnames(mm))]
 colnames(mm)
 fake <- vector()
+values <- vector()
 head(mm)
 
 for(i in 1:nsp){ # loop over species, as these are the random effect modeled
@@ -143,7 +145,7 @@ for(i in 1:nsp){ # loop over species, as these are the random effect modeled
              rnorm(1, photochill1, photochill1.sd)
              # rnorm(1, photochill2, photochill2.sd)
   )
-  
+  values <- rbind(values, coeff)
   bb <- rnorm(n = length(warm), mean = mm %*% coeff, sd = 0.1)
   
   fakex <- data.frame(bb, sp = i, site, warm, photo, chillP)
@@ -151,8 +153,12 @@ for(i in 1:nsp){ # loop over species, as these are the random effect modeled
   
   fake <- rbind(fake, fakex)  
 }
+head(values)
 head(fake)
-summary(lm(bb ~ (site + warm + photo + chillP)^2, data = fake)) # sanity check 
+
+values <- as.data.frame(values)
+names(values) <- c("int","b.site","b.force", "b.photo","b.chill1","b.fs","b.ps","b.cs","b.fp","b.fc", "b.pc")
+#summary(lm(bb ~ (site + warm + photo + chillP)^2, data = fake)) # sanity check 
 
 plot(fake$bb ~ fake$chillP)
 abline(35,-5)
@@ -175,7 +181,7 @@ fake$photo <- as.numeric(fake$photo)
 fake$photo[fake$photo==1] <- 0
 fake$photo[fake$photo==2] <- 1
 
-summary(lm(bb ~ (site+warm+photo+chill)^2, data = fake)) # double sanity check 
+#summary(lm(bb ~ (site+warm+photo+chill)^2, data = fake)) # double sanity check 
 
 #summary(lmer(bb ~ (site|sp) + (warm|sp) + (photo|sp) + (chill1|sp) + (chill2|sp), data = fake)) # too hard for lmer.
 
@@ -207,13 +213,13 @@ mdl.full <- stan("stan/df_interdirect.stan",
                  data = datalist,
                  include = FALSE, pars = c("ypred_new","y_hat"),
                  iter = 4000, chains= 4, warmup = 2000)
-save(mdl.full, file = "output/df_interdirect.Rda")
+save(mdl.full, file = "output/df_interdirect_plot.Rda")
 
-load("output/df_interdirect.Rda")
-#  load("output/tbb_ncp_chillportions_zsc_dl.Rda")
-# #
-ssm <-  as.shinystan(mdl.full)
-launch_shinystan(ssm)
+# load("output/df_interdirect.Rda")
+# #  load("output/tbb_ncp_chillportions_zsc_dl.Rda")
+# # #
+# ssm <-  as.shinystan(mdl.full)
+# launch_shinystan(ssm)
 # # # 
 sum <- summary(mdl.full)$summary 
 summary(mdl.full)$summary[c("mu_a", "mu_b_warm","mu_b_chill1","mu_b_photo","mu_b_site","mu_b_inter_wp","mu_b_inter_ps","mu_b_inter_wc1","mu_b_inter_pc1","mu_b_inter_sc1","sigma_b_warm","sigma_b_chill1","sigma_b_photo","sigma_b_site","sigma_a","sigma_b_inter_wp","sigma_b_inter_ps","sigma_b_inter_wc1","sigma_b_inter_pc1","sigma_b_inter_sc1", "sigma_y"),"mean"]
@@ -222,17 +228,27 @@ summary(mdl.full)$summary[c("mu_a", "mu_b_warm","mu_b_chill1","mu_b_photo","mu_b
 
 intSp <- as.data.frame(sum[grep("a_sp", rownames(sum)), ]) 
 slopeF <- as.data.frame(sum[grep("b_warm", rownames(sum)), ]); slopeF <- slopeF[c(1:28),]
-slopeC <- as.data.frame(sum[grep("b_chill", rownames(sum)), ]) 
-slopeP <- as.data.frame(sum[grep("b_photo", rownames(sum)), ]) 
-slopeFC <- as.data.frame(sum[grep("b_inter_wc", rownames(sum)), ]) 
-slopeCP <- as.data.frame(sum[grep("b_inter_cp", rownames(sum)), ]) 
-slopeFP <- as.data.frame(sum[grep("b_inter_wp", rownames(sum)), ]) ; slopeFP <- slopeFP[c(9:16),]
+slopeC <- as.data.frame(sum[grep("b_chill1", rownames(sum)), ]) ; slopeC <- slopeC[c(1:28),]
+slopeP <- as.data.frame(sum[grep("b_photo", rownames(sum)), ]) ; slopeP <- slopeP[c(1:28),]
+slopeSite <- as.data.frame(sum[grep("b_site", rownames(sum)), ]) ; slopeSite <- slopeSite[c(1:28),]
+slopeFP <- as.data.frame(sum[grep("b_inter_wp", rownames(sum)), ]) ; slopeFP <- slopeFP[c(1:28),]
+slopeFC <- as.data.frame(sum[grep("b_inter_wc1", rownames(sum)), ]) ; slopeFC <- slopeFC[c(1:28),]
+slopeCP <- as.data.frame(sum[grep("b_inter_pc1", rownames(sum)), ]) ; slopeCP <- slopeCP[c(1:28),]
+slopeFS <- as.data.frame(sum[grep("b_inter_ws", rownames(sum)), ]) ; slopeFS <- slopeFS[c(1:28),]
+slopePS <- as.data.frame(sum[grep("b_inter_ps", rownames(sum)), ]) ; slopePS <- slopePS[c(1:28),]
+slopeCS <- as.data.frame(sum[grep("b_inter_sc1", rownames(sum)), ]) ; slopeCS <- slopeCS[c(1:28),]
 
-
-par(mfrow = c(4,2))
-plot( rnorm(nsp, warmdiff, warmdiff.sd) ~ slopeF$mean, pch = 19)
-plot(alpha.chill.sp ~ slopeC$mean, pch = 19)
-plot(alpha.photo.sp ~ slopeP$mean, pch = 19)
-plot(alpha.fc ~ slopeFC$mean, pch = 19)
-plot(alpha.cp ~ slopeCP$mean, pch = 19)
-plot(alpha.fp ~ slopeFP$mean, pch = 19)
+pdf("fake_esti_correlations.pdf", height = 10, width = 5)
+par(mfrow = c(6,2))
+plot(values$int ~ intSp$mean, pch =19)
+plot( values$b.force ~ slopeF$mean, pch = 19)
+plot(values$b.chill1 ~ slopeC$mean, pch = 19)
+plot(values$b.photo ~ slopeP$mean, pch = 19)
+plot(values$b.fc ~ slopeFC$mean, pch = 19)
+plot(values$b.pc ~ slopeCP$mean, pch = 19)
+plot(values$b.fp ~ slopeFP$mean, pch = 19)
+plot(values$b.fs ~ slopeFS$mean, pch = 19)
+plot(values$b.cs ~ slopeCS$mean, pch = 19)
+plot(values$b.ps ~ slopePS$mean, pch = 19)
+plot(values$b.site ~ slopeSite$mean, pch = 19)
+dev.off()

83a482eb61c143980f466a58fabb62d7b86dc660
DeirdreLoughnan <deirdre.loughnan@gmail.com>
2022-03-02 15:26:16 -0800

    worked on ewtransect code

--- rcode/bc_bb_ewtransect.R
+++ rcode/bc_bb_ewtransect.R
@@ -5,8 +5,16 @@
 
 # Adding standardization: following the methods outlined by Andrew Gelman in his paper
 
-# To properly add site to the model I am going to use the indexing approach used on pg 153 of statistical rethinking, modeling code was drafted by Lizzie
+# changing from site to transect, with transect added as a 0/1 dummy variable, west = 0, east = 1
 
+#Feb 28: model consistently has 7 divergent transitions, these persisted when I increased the iterations to 3000 warmup, 4000 iterations, and increased the adapt delta to 0.99 - from the log posterior vs parameter plots, the worst are sigma_chill with them all butting up against one side, sigma_b_inter_pc -- I am going to try ncp for chilling as well as photo and see if that helps
+
+# March 1: the ncp for chilling produced 79 divergent transitions. The fact that this made it worse got me thinking that maybe the issue is the model has more complexity then it needs, so I removed the ncp on photo
+
+# the model with no ncp on photo or chilling produced dt, running it for 5000 warmup and 6000 iterations produced only 1 dt but the rhat were bad at 1.13
+
+# next I tried running no ncp on photo and chill, but with a bigger adapt delta, 4000:3000 iterations, there were no dt, but the rhat was really bad at 1.39 and the max tree depth exceeded
+# 
 #library(scales)
 #library(arm)
 library(rstan)
@@ -17,7 +25,7 @@ library(ggplot2)
 #library(RColorBrewer)
 library(dplyr)
 library(plyr)
-library(tidybayes)
+#library(tidybayes)
 
 options(mc.cores = parallel::detectCores())
 
@@ -112,11 +120,25 @@ datalist_z <- list( N=nrow(pheno.t),
                     force = pheno.t$force.z2,
                     site = pheno.t$transect.n)
 
-mdl <- stan("stan/bc.bb.ncpphoto.ncpinter.standardize.old.stan",
-              data = datalist_z,
+mdl <- stan("stan/bc.bb.noncp.standardize.old.stan",
+            data = datalist_z,
             include = FALSE, pars = c("ypred_new","y_hat"),
-              iter = 4000, warmup = 3000, chains=4, control = list(adapt_delta = 0.99))
-save(mdl, file="output/tbb_cport_stnd_transect.Rda")
+            iter = 6000, warmup = 5000, chains=4,
+            control = list(adapt_delta= 0.99))
+save(mdl, file="output/tbb_cport_stnd_transect_noncp_moreit.Rda")
+# 
+mdl <- stan("stan/bc.bb.noncp.standardize.old.stan",
+            data = datalist_z,
+            include = FALSE, pars = c("ypred_new","y_hat"),
+            iter = 4000, warmup = 3000, chains=4,
+            control = list(adapt_delta= 0.99, max_treedepth = 15))
+save(mdl, file="output/tbb_cport_stnd_transect_noncpadapt.Rda")
+
+# mdl <- stan("stan/bc.bb.ncpphotochill.ncpinter.standardize.old.stan",
+#               data = datalist_z,
+#             include = FALSE, pars = c("ypred_new","y_hat"),
+#               iter = 4000, warmup = 3000, chains=4)
+# save(mdl, file="output/tbb_cport_stnd_transect_ncpchillphoto.Rda")
 
 # mdl.i <- stan("stan/bc_bb_ncpphoto_ncpinter_standardize_index.stan",
 #               data = datalist_z,
@@ -130,12 +152,152 @@ save(mdl, file="output/tbb_cport_stnd_transect.Rda")
 # save(mdl.d, file="output/tbb_cport_stnd_dummy.Rds")
 #######################################################################
 
-load("output/tbb_cport_stnd_transect.Rda")
-# #  load("output/tbb_ncp_chillportions_zsc_dl.Rda")
+# load("output/tbb_cport_stnd_transect_ncpchillphoto.Rda")
+# # #  load("output/tbb_ncp_chillportions_zsc_dl.Rda")
+# # # #
+# ssm <-  as.shinystan(mdl)
+# launch_shinystan(ssm)
 # # #
-ssm <-  as.shinystan(mdl)
-launch_shinystan(ssm)
-# # # 
 sum <- summary(mdl)$summary 
 summary(mdl)$summary[c("mu_a", "mu_force","mu_chill","mu_photo","mu_site","mu_inter_fp", "mu_inter_fs","mu_inter_ps","mu_inter_fc","mu_inter_pc","mu_inter_sc","sigma_a", "sigma_force","sigma_chill","sigma_photo","sigma_site","sigma_b_inter_fp","sigma_b_inter_fs","sigma_b_inter_ps","sigma_b_inter_fc","sigma_b_inter_pc","sigma_b_inter_sc", "sigma_y"),"mean"]
 
+# pairs(mdl, pars = c("mu_a", "mu_force","mu_chill","mu_photo","mu_site","mu_inter_fp", "mu_inter_fs","mu_inter_ps","mu_inter_fc","mu_inter_pc","mu_inter_sc", "lp__"))
+# 
+# pairs(mdl, pars = c("sigma_a", "sigma_force","sigma_chill","sigma_photo","sigma_site","sigma_b_inter_fp","sigma_b_inter_fs","sigma_b_inter_ps","sigma_b_inter_fc","sigma_b_inter_pc","sigma_b_inter_sc", "sigma_y", "lp__"))
+
+
+
+# post <- rstan::extract(mdl)
+# # histograms
+# par(mfrow = c(1,1))
+# h1 <- hist(rnorm(1000, 1, 35))
+# h2 <- hist(post$mu_a)
+# plot(h2, col=rgb(0,0,1,1/4), xlim =c(-100,100))
+# plot(h1, col=rgb(1,0,1,1/4), add = TRUE)
+# 
+# # mu_force
+# h1 <- hist(rnorm(1000, 0, 50))
+# h2 <- hist(post$mu_force)
+# plot(h2, col=rgb(0,0,1,1/4), xlim =c(-150,150))
+# plot(h1, col=rgb(1,0,1,1/4), add = TRUE)
+# 
+# # mu_chill
+# h1 <- hist(rnorm(1000, 0, 35))
+# h2 <- hist(post$mu_chill)
+# plot(h2, col=rgb(0,0,1,1/4), xlim =c(-100,100))
+# plot(h1, col=rgb(1,0,1,1/4), add = TRUE)
+# 
+# # mu_photo
+# h1 <- hist(rnorm(1000, 0, 35))
+# h2 <- hist(post$mu_photo)
+# plot(h2, col=rgb(0,0,1,1/4), xlim =c(-100,100))
+# plot(h1, col=rgb(1,0,1,1/4), add = TRUE)
+# 
+# # mu_site
+# h1 <- hist(rnorm(1000, 1, 35))
+# h2 <- hist(post$mu_site)
+# plot(h2, col=rgb(0,0,1,1/4), xlim =c(-100,100))
+# plot(h1, col=rgb(1,0,1,1/4), add = TRUE)
+# 
+# # mu_inter_fp
+# h1 <- hist(rnorm(1000, 0, 35))
+# h2 <- hist(post$mu_inter_fp)
+# plot(h2, col=rgb(0,0,1,1/4), xlim =c(-100,100))
+# plot(h1, col=rgb(1,0,1,1/4), add = TRUE)
+# 
+# # mu_inter_fs
+# h1 <- hist(rnorm(1000, 0, 35))
+# h2 <- hist(post$mu_inter_fs)
+# plot(h2, col=rgb(0,0,1,1/4), xlim =c(-100,100))
+# plot(h1, col=rgb(1,0,1,1/4), add = TRUE)
+# 
+# # mu_inter_fc
+# h1 <- hist(rnorm(1000, 0, 35))
+# h2 <- hist(post$mu_inter_fc)
+# plot(h2, col=rgb(0,0,1,1/4), xlim =c(-100,100))
+# plot(h1, col=rgb(1,0,1,1/4), add = TRUE)
+# 
+# # mu_inter_ps
+# h1 <- hist(rnorm(1000, 0, 35))
+# h2 <- hist(post$mu_inter_ps)
+# plot(h2, col=rgb(0,0,1,1/4), xlim =c(-100,100))
+# plot(h1, col=rgb(1,0,1,1/4), add = TRUE)
+# 
+# # mu_inter_pc
+# h1 <- hist(rnorm(1000, 0, 35))
+# h2 <- hist(post$mu_inter_pc)
+# plot(h2, col=rgb(0,0,1,1/4), xlim =c(-100,100))
+# plot(h1, col=rgb(1,0,1,1/4), add = TRUE)
+# 
+# # mu_inter_sc
+# h1 <- hist(rnorm(1000, 0, 35))
+# h2 <- hist(post$mu_inter_sc)
+# plot(h2, col=rgb(0,0,1,1/4), xlim =c(-100,100))
+# plot(h1, col=rgb(1,0,1,1/4), add = TRUE)
+# 
+# # sigma_a
+# h1 <- hist(rnorm(1000, 0, 10))
+# h2 <- hist(post$sigma_a)
+# plot(h2, col=rgb(0,0,1,1/4), xlim =c(-50,50))
+# plot(h1, col=rgb(1,0,1,1/4), add = TRUE)
+# 
+# # sigma_force
+# h1 <- hist(rnorm(1000, 0, 10))
+# h2 <- hist(post$sigma_force)
+# plot(h2, col=rgb(0,0,1,1/4), xlim =c(-50,50))
+# plot(h1, col=rgb(1,0,1,1/4), add = TRUE)
+# 
+# # sigma_chill
+# h1 <- hist(rnorm(1000, 0, 40))
+# h2 <- hist(post$sigma_chill)
+# plot(h2, col=rgb(0,0,1,1/4), xlim =c(-100,100))
+# plot(h1, col=rgb(1,0,1,1/4), add = TRUE)
+# 
+# # sigma_photo
+# h1 <- hist(rnorm(1000, 0, 10))
+# h2 <- hist(post$sigma_photo)
+# plot(h2, col=rgb(0,0,1,1/4), xlim =c(-50,50))
+# plot(h1, col=rgb(1,0,1,1/4), add = TRUE)
+# 
+# # sigma_site
+# h1 <- hist(rnorm(1000, 0, 40))
+# h2 <- hist(post$sigma_site)
+# plot(h2, col=rgb(0,0,1,1/4), xlim =c(-100,100))
+# plot(h1, col=rgb(1,0,1,1/4), add = TRUE)
+# 
+# # sigma_interaction
+# h1 <- hist(rnorm(1000, 1, 10))
+# h2 <- hist(post$sigma_b_inter_fp, xlim =c(-10,10))
+# plot(h2, col=rgb(0,0,1,1/4), xlim =c(-100,100))
+# plot(h1, col=rgb(1,0,1,1/4), add = TRUE)
+# 
+# h1 <- hist(rnorm(1000, 1, 10))
+# h2 <- hist(post$sigma_b_inter_fs, xlim =c(-10,10))
+# plot(h2, col=rgb(0,0,1,1/4), xlim =c(-100,100))
+# plot(h1, col=rgb(1,0,1,1/4), add = TRUE)
+# 
+# h1 <- hist(rnorm(1000, 1, 10))
+# h2 <- hist(post$sigma_b_inter_ps, xlim =c(-10,10))
+# plot(h2, col=rgb(0,0,1,1/4), xlim =c(-100,100))
+# plot(h1, col=rgb(1,0,1,1/4), add = TRUE)
+# 
+# h1 <- hist(rnorm(1000, 1, 10))
+# h2 <- hist(post$sigma_b_inter_fc, xlim =c(-10,10))
+# plot(h2, col=rgb(0,0,1,1/4), xlim =c(-100,100))
+# plot(h1, col=rgb(1,0,1,1/4), add = TRUE)
+# 
+# h1 <- hist(rnorm(1000, 1, 10))
+# h2 <- hist(post$sigma_b_inter_pc, xlim =c(-10,10))
+# plot(h2, col=rgb(0,0,1,1/4), xlim =c(-100,100))
+# plot(h1, col=rgb(1,0,1,1/4), add = TRUE)
+# 
+# h1 <- hist(rnorm(1000, 1, 20))
+# h2 <- hist(post$sigma_b_inter_sc, xlim =c(-10,10))
+# plot(h2, col=rgb(0,0,1,1/4), xlim =c(-100,100))
+# plot(h1, col=rgb(1,0,1,1/4), add = TRUE)
+# 
+# h1 <- hist(rnorm(1000, 1, 10))
+# h2 <- hist(post$sigma_y, xlim =c(-10,10))
+# plot(h2, col=rgb(0,0,1,1/4), xlim =c(-100,100))
+# plot(h1, col=rgb(1,0,1,1/4), add = TRUE)
+# 
